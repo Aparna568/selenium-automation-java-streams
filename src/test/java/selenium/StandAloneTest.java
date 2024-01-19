88eb3b498a7b7f6f8bda281718721b378fafab55
aparna <aparna9imakaa@gmail.com>
2024-01-19 14:03:50 -0500

    end-to-end

--- src/test/java/selenium/StandAloneTest.java
+++ src/test/java/selenium/StandAloneTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -33,11 +34,21 @@ driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	
 	WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
 	wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("toast-container")));
-	wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("ng-animating")));
+	wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating")))) ;    
 	driver.findElement(By.xpath("//button[@routerlink='/dashboard/cart']")).click();
 	List<WebElement> cartItem = driver.findElements(By.xpath("//div[@class='cartSection']//h3"));
 	Boolean match = cartItem.stream().anyMatch(s->s.getText().equalsIgnoreCase(productName));
 	
 	Assert.assertTrue(match);
+	driver.findElement(By.xpath("//button[text()='Checkout']")).click();
+	Actions action = new Actions(driver);
+	action.sendKeys(driver.findElement(By.xpath("//input[@placeholder='Select Country']")),"India").build().perform();
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//section[@class='ta-results list-group ng-star-inserted']")));
+	driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+	driver.findElement(By.xpath("//a[@class='btnn action__submit ng-star-inserted']")).click();
+	String message = driver.findElement(By.cssSelector(".hero-primary")).getText();
+	System.out.println(message);
+	Assert.assertTrue(message.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+	driver.quit();
 }
 }
